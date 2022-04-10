1a0f351c7c8a231fa36b3731a2f81678aa0aad54
ruben <rubendasilva@me.com>
2022-04-09 17:39:04 -0700

    Deck, shuffle(), deal(), reset() added

--- js/app.js
+++ js/app.js
@@ -4,18 +4,22 @@ class Deck {
 
         const cardColor = ['green', 'red'];
         const staves = ['circles', 'triangles', 'squares'];
-        const faceValue = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        const posFaceValue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        const negFaceValue = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1];
 
         for (let suit in staves) {
-            for (let value in faceValue) {
-                for (let color in cardColor) {
-                    this.deck.push(`${cardColor[color]} ${staves[suit]} ${faceValue[value]}`);
-                }
+            for (let value in posFaceValue) {
+                this.deck.push(`green ${staves[suit]} ${posFaceValue[value]}`);
             }
-        this.deck.push(`sylop 0`, `sylop 0`);
         }
-    }
-
+        for (let suit in staves) {
+            for (let value in negFaceValue) {
+                this.deck.push(`red ${staves[suit]} ${negFaceValue[value]}`);
+            }
+        }
+        this.deck.push(`sylop 0`, `sylop 0`);
+        
+    }   
     shuffle() {
         const deck = this.deck;
         let cardTotal = deck.length;
@@ -37,16 +41,20 @@ class Deck {
 
         const cardColor = ['green', 'red'];
         const staves = ['circles', 'triangles', 'squares'];
-        const faceValue = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        const posFaceValue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        const negFaceValue = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1];
 
         for (let suit in staves) {
-            for (let value in faceValue) {
-                for (let color in cardColor) {
-                    this.deck.push(`${cardColor[color]} ${staves[suit]} ${faceValue[value]}`);
-                }
+            for (let value in posFaceValue) {
+                this.deck.push(`green ${staves[suit]} ${posFaceValue[value]}`);
             }
-        this.deck.push(`sylop 0`, `sylop 0`);
         }
+        for (let suit in staves) {
+            for (let value in negFaceValue) {
+                this.deck.push(`red ${staves[suit]} ${negFaceValue[value]}`);
+            }
+        }
+        this.deck.push(`sylop 0`, `sylop 0`);
     }
 }
 
@@ -54,34 +62,3 @@ const deck1 = new Deck();
 console.log(deck1.deck);
 deck1.shuffle();
 console.log(deck1.deck);
-// deck1.deal();
-// console.log(deck1.deck);
-
-// const testDeck = (colorArray, suitArray, valueArray) => {
-//     value.forEach((element, index)  => {
-//         const card = {
-//             valueArray[index] : staves[index].concat(cardColor[index]),
-//         }
-//         return card;
-//     });
-// }
-// console.log(testDeck(faceValue, staves, cardColor ));
-//     grnCircleOne : 1, 
-//     grnCircleTwo : 2, 
-//     grnCircleThr : 3, 
-//     grnCircleFou : 4, 
-//     grnCircleFiv : 5, 
-//     grnCircleSix : 6, 
-//     grnCircleSev : 7, 
-//     grnCircleEig : 8, 
-//     grnCircleNin : 9, 
-//     grnCircleTen : 10, 
-//     redCircleOne : -1, 
-//     redCircleTwo : -2, 
-//     redCircleThr : -3, 
-//     redCircleFou : -4, 
-//     redCircleFiv : -5, 
-//     redCircleSix : -6, 
-//     redCircleSev : -7, 
-//     redCircleEig : -8, 
-//     redCircleNin : -9, 
