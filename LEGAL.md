916dec15f2cd1632137ef2dfe5fea8d1a475c54d
ruben <rubendasilva@me.com>
2022-04-09 20:59:54 -0700

    README.md updated

--- LEGAL.md
+++ LEGAL.md
@@ -1,3 +1,10 @@
+All reused work maintains its original copyright where applicable and is used here with the assertion that it is fair use of the material under United States copyright law. Original text and images are licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.
+https://en.wikipedia.org/wiki/Fair_use
+https://en.wikipedia.org/wiki/Copyright_law_of_the_United_States
+http://creativecommons.org/licenses/by-sa/4.0/
+
+
+
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
--- js/app.js
+++ js/app.js
@@ -2,24 +2,24 @@ class Deck {
     constructor() {
         this.deck = [];
 
-        const cardColor = ['green', 'red'];
-        const staves = ['circles', 'triangles', 'squares'];
-        const posFaceValue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        const negFaceValue = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1];
+        const staves = ['circles', 'triangles', 'squares'];//three suits of green and red, 60 cards
+        const greenFaceValue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];//all green cards are of positive values, 30 green cards
+        const redFaceValue = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1];//all red cards are of positive values, 30 red cards
 
         for (let suit in staves) {
-            for (let value in posFaceValue) {
-                this.deck.push(`green ${staves[suit]} ${posFaceValue[value]}`);
+            for (let value in greenFaceValue) {
+                this.deck.push(`green ${staves[suit]} ${greenFaceValue[value]}`);//all three suits given positive values and 'green' added to name
             }
         }
         for (let suit in staves) {
-            for (let value in negFaceValue) {
-                this.deck.push(`red ${staves[suit]} ${negFaceValue[value]}`);
+            for (let value in redFaceValue) {
+                this.deck.push(`red ${staves[suit]} ${redFaceValue[value]}`);//all three suits given negative values and 'red' added to name
             }
         }
-        this.deck.push(`sylop 0`, `sylop 0`);
+        this.deck.push(`sylop 0`, `sylop 0`);//only two sylops of 0 value and of no suit per deck, 62 cards in total
         
-    }   
+    }
+    //shuffles created deck
     shuffle() {
         const deck = this.deck;
         let cardTotal = deck.length;
@@ -27,31 +27,30 @@ class Deck {
 
         while (cardTotal) {
             random = Math.floor(Math.random() * cardTotal--);
-            [deck[cardTotal], deck[random]] = [deck[random], deck[cardTotal]];
+            [deck[cardTotal], deck[random]] = [deck[random], deck[cardTotal]];//Fisher-Yates Shuffle https://bost.ocks.org/mike/shuffle/
         }
         return this;
     }
-
+    //removes one card from created deck
     deal() {
         return this.deck.pop();
     }
-
+    //unshuffles created deck (copy of constructor)
     reset(){
         this.deck = [];
 
-        const cardColor = ['green', 'red'];
         const staves = ['circles', 'triangles', 'squares'];
-        const posFaceValue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        const negFaceValue = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1];
+        const greenFaceValue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        const redFaceValue = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1];
 
         for (let suit in staves) {
-            for (let value in posFaceValue) {
-                this.deck.push(`green ${staves[suit]} ${posFaceValue[value]}`);
+            for (let value in greenFaceValue) {
+                this.deck.push(`green ${staves[suit]} ${greenFaceValue[value]}`);
             }
         }
         for (let suit in staves) {
-            for (let value in negFaceValue) {
-                this.deck.push(`red ${staves[suit]} ${negFaceValue[value]}`);
+            for (let value in redFaceValue) {
+                this.deck.push(`red ${staves[suit]} ${redFaceValue[value]}`);
             }
         }
         this.deck.push(`sylop 0`, `sylop 0`);
@@ -61,4 +60,6 @@ class Deck {
 const deck1 = new Deck();
 console.log(deck1.deck);
 deck1.shuffle();
+console.log(deck1.deck);
+deck1.deal();
 console.log(deck1.deck);
