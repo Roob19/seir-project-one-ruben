1c69d7babad1876ce5a445dd0488f30db2614a22
ruben <rubendasilva@me.com>
2022-04-14 12:40:24 -0700

    cleaned up

--- pu.sh
+++ pu.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 git add -A
-echo  "<><><><><><><><><><><><>"
+echo "<><><><><><><><><><><><>"
 echo
 echo "Enter commit message: "
 read cmtMsg
 git commit -m "$cmtMsg"
-echo  "<><><><><><><><><><><>"
+echo "<><><><><><><><><><><>"
 echo
 echo “Push to which branch?”
 read pushBranch
 git push -u origin $pushBranch
-echo  "<><><><><><><><><><><>”
+echo "<><><><><><><><><><><>”
 echo
-echo " Push complete :) "
+echo "Push complete :)"
 echo
-echo  "<><><><><><><><><><><>”
+echo "<><><><><><><><><><><>”
